10c29880b956c49768ca853d082d4768fc02d8d4
pablo <pnunfe@gmail.com>
2025-03-09 23:33:33 +0100

    refactor: optimize PokemonDetailsComponent data fetching with switchMap for improved readability and performance

--- src/app/components/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon-details/pokemon-details.component.ts
@@ -3,6 +3,7 @@ import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { switchMap } from 'rxjs/operators';
 import { Pokemon } from 'src/app/interfaces/pokemons.interface';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
@@ -19,29 +20,29 @@ export class PokemonDetailsComponent implements OnInit {
   showError = inject(MessageService);
 
   ngOnInit(): void {
-    // We get the id from the url
     this.route.paramMap
-      // We unsubscribe from the observable when the component is destroyed
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((params) => {
-        const id = params.get('id');
-        // if the id is not null, we fetch the pokemon details
-        if (id) {
-          const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-          this.pokemonService
-            .fetchPokemonDetails(url)
-            .pipe(takeUntilDestroyed(this.destroyRef))
-            .subscribe({
-              next: (data) => this.pokemonDetails.set(data),
-              error: () => {
-                this.showError.add({
-                  severity: 'error',
-                  summary: 'Error',
-                  detail: 'Failed to fetch pokemon details',
-                });
-              },
-            });
-        }
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        // We get the id from the url
+        switchMap((params) => {
+          const id = params.get('id');
+          // if the id is not null, we fetch the pokemon details
+          if (id) {
+            const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+            return this.pokemonService.fetchPokemonDetails(url);
+          }
+          return [];
+        })
+      )
+      .subscribe({
+        next: (data) => this.pokemonDetails.set(data),
+        error: () => {
+          this.showError.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Failed to fetch pokemon details',
+          });
+        },
       });
   }
 }
